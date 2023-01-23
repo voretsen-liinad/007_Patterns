65254f15b738281e49cb4f9d1844706ad5f73f69
voretsen-liinad <maidanov1991@yandex.ru>
2023-01-23 23:40:25 +0500

    L01

---
+++ 1/SBomberProject/SBomberProject/House.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+#include "House.h"
+#include "MyTools.h"
+
+using namespace std;
+using namespace MyTools;
+
+bool House::isInside(double x1, double x2) const
+{
+	const double XBeg = x + 2;
+	const double XEnd = x + width - 1;
+
+	if (x1 < XBeg && x2 > XEnd)
+	{
+		return true;
+	}
+
+	if (x1 > XBeg && x1 < XEnd)
+	{
+		return true;
+	}
+
+	if (x2 > XBeg && x2 < XEnd)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void House::Draw() const
+{
+	MyTools::ScreenSingleton::getInstance().SetColor(CC_Yellow);
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y - 5);
+	cout << "  ########  ";
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y - 4);
+	cout << "##        ##";
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y - 3);
+	cout << "############";
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y - 2);
+	cout << "#          #";
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y - 1);
+	cout << "#          #";
+	MyTools::ScreenSingleton::getInstance().GotoXY(x, y);
+	cout << "############";
+}
