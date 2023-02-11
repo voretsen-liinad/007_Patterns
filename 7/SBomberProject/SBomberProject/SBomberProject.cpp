75ef754f1e2f7f61796dd8729b0e1e024161f9d8
voretsen-liinad <maidanov1991@yandex.ru>
2023-02-11 14:27:21 +0500

    L07"
    exit

---
+++ 7/SBomberProject/SBomberProject/SBomberProject.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <conio.h>
+
+#include "SBomber.h"
+#include "MyTools.h"
+
+using namespace std;
+
+//========================================================================================================================
+
+MyTools::FileLogger logger("log.txt"); //a definition of the class "FileLogger" is in the files "MyTools.h" and "MyTools.cpp"
+
+int main(void)
+{
+
+    SBomber game;
+
+    do {
+        game.TimeStart();
+
+        if (_kbhit())
+        {
+            game.ProcessKBHit();
+        }
+
+        MyTools::ClrScr();
+
+        game.DrawFrame();
+        game.MoveObjects();
+        game.CheckObjects();
+
+        game.TimeFinish();
+
+    } while (!game.GetExitFlag());
+
+    return 0;
+}
